27c0e80f1751b38ee64062694e2122601dcdeded
Louis Yu <louis@did.id>
2023-06-28 00:31:38 +0800

    fix: use png for emojis

--- src/lib/ui/ChooseEmoji/index.tsx
+++ src/lib/ui/ChooseEmoji/index.tsx
@@ -1,56 +1,39 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Button, ButtonShape, ButtonSize } from '../../components'
 import { useSimpleRouter } from '../../components/SimpleRouter'
 import clsx from 'clsx'
-
-export const EmojiList = [
-  '📱',
-  '🖥️',
-  '💻',
-  '💾',
-  '🍟',
-  '🙂',
-  '🌺',
-  '🎁',
-  '🐭',
-  '😍',
-  '🌻',
-  '🌭',
-  '🎂',
-  '🐹',
-  '😎',
-  '🌞',
-  '🌮',
-  '🍰',
-  '🐰',
-  '😉',
-  '🌕',
-  '🍦',
-]
+import { emojis } from './png'
 
 export function ChooseEmoji() {
   const goNext = useSimpleRouter()?.goNext
   const [selected, setSelected] = useState<string>()
+  const onClick = useCallback((k: string) => () => setSelected(k), [setSelected])
   return (
     <div className="flex w-full max-w-[400px] flex-col items-center justify-start px-6 py-20">
       <div className="w-full text-center text-[16px] leading-normal text-neutral-700">
         Choose an Emoji for the new device for easy identification.
       </div>
-      <div className="mt-6 grid w-full grid-cols-6  rounded-2xl border border-stone-300 border-opacity-20 bg-gray-50 p-2">
-        {EmojiList.map((emoji) => (
+      <div className="mt-6 grid w-full grid-cols-6 rounded-2xl border border-stone-300 border-opacity-20 bg-gray-50 p-2">
+        {Object.entries(emojis).map(([k, v]) => (
           <div
-            onClick={() => setSelected(emoji)}
-            key={emoji}
+            onClick={onClick(k)}
+            key={k}
             className={clsx(
-              'relative box-border h-12 w-12 cursor-pointer select-none rounded-xl hover:bg-slate-600/10 active:bg-slate-600/20',
-              selected === emoji && 'border-2 border-emerald-400',
+              'relative box-border h-12 w-12 cursor-pointer select-none rounded-xl transition-none hover:bg-slate-600/10 active:bg-slate-600/20',
+              selected === k && 'border-2 border-emerald-400 bg-white',
             )}
           >
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[32px]">{emoji}</div>
+            <img className="absolute left-1/2 top-1/2 w-8 max-w-none -translate-x-1/2 -translate-y-1/2" src={v} />
           </div>
         ))}
       </div>
-      <Button shape={ButtonShape.round} size={ButtonSize.middle} className="mt-7 w-full px-5" onClick={goNext}>
+      <Button
+        disabled={selected === undefined}
+        shape={ButtonShape.round}
+        size={ButtonSize.middle}
+        className="mt-7 w-full px-5"
+        onClick={goNext}
+      >
         Next
       </Button>
     </div>
---
+++ src/lib/ui/ChooseEmoji/png/index.tsx
@@ -0,0 +1,51 @@
+import apple from './apple.png'
+import basketball from './basketball.png'
+import cat from './cat.png'
+import computer from './computer.png'
+import crown from './crown.png'
+import dice from './dice.png'
+import doughnut from './doughnut.png'
+import fire from './fire.png'
+import frog from './frog.png'
+import heart from './heart.png'
+import laptop from './laptop.png'
+import pad from './pad.png'
+import phone from './phone.png'
+import pig from './pig.png'
+import rainbow from './rainbow.png'
+import shark from './shark.png'
+import smiling from './smiling.png'
+import snowman from './snowman.png'
+import soccer from './soccer.png'
+import sparkles from './sparkles.png'
+import swan from './swan.png'
+import unicorn from './unicorn.png'
+import voltage from './voltage.png'
+import watermelon from './watermelon.png'
+
+export const emojis = {
+  apple,
+  basketball,
+  cat,
+  computer,
+  crown,
+  dice,
+  doughnut,
+  fire,
+  frog,
+  heart,
+  laptop,
+  pad,
+  phone,
+  pig,
+  rainbow,
+  shark,
+  smiling,
+  snowman,
+  soccer,
+  sparkles,
+  swan,
+  unicorn,
+  voltage,
+  watermelon,
+}
