448912e541e25f04d87b9fa0a1ce6871e375c345
Louis Yu <louis@did.id>
2023-07-12 00:01:10 +0800

    feat: added preact

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
 import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
 import tailwindcss from 'tailwindcss'
 import { UserConfigExport } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
+import alias from '@rollup/plugin-alias'
 import { name } from './package.json'
 
 const app = async (): Promise<UserConfigExport> => {
   return defineConfig({
     plugins: [
-      react(),
-      dts({
-        insertTypesEntry: true,
+      alias({
+        entries: [
+          { find: 'react', replacement: 'preact/compat' },
+          { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+          { find: 'react-dom', replacement: 'preact/compat' },
+          { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+        ],
       }),
+      preact(),
+      // dts({
+      //   insertTypesEntry: true,
+      // }),
       // visualizer({
       //   template: "treemap", // or sunburst
       //   open: true,
@@ -36,14 +46,14 @@ const app = async (): Promise<UserConfigExport> => {
       },
       rollupOptions: {
         treeshake: 'smallest',
-        external: ['react', 'react-dom', 'tailwindcss'],
-        output: {
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            tailwindcss: 'tailwindcss',
-          },
-        },
+        // external: ['react', 'react-dom', 'tailwindcss'],
+        // output: {
+        //   globals: {
+        //     react: 'React',
+        //     'react-dom': 'ReactDOM',
+        //     tailwindcss: 'tailwindcss',
+        //   },
+        // },
       },
     },
     test: {
