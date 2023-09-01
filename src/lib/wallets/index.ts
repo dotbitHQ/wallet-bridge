f5e735aecf4209dc19708250ad1b05c2be5fbbba
web3max <longhuicode@foxmail.com>
2023-09-01 15:22:44 +0800

    ci: github action.

--- src/lib/wallets/index.ts
+++ src/lib/wallets/index.ts
@@ -243,7 +243,12 @@ class WalletSDK {
       signData: async (data: SignDataType, options?: Record<string, any>): Promise<string | undefined> => {
         return await this.signData(data, { ...options, provider })
       },
-      onFailed: provider?.onFailed,
+      onFailed:
+        provider?.onFailed ||
+        (async (k: any): Promise<any> => {
+          console.log(k)
+          return undefined
+        }),
     }
   }
 
