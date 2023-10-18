825893e421edecd59776d1df0bb19650c06ea145
leon <tianxiemaochiyu@outlook.com>
2023-10-18 18:16:28 +0800

    fix: render bug

--- src/lib/ui/ConnectWallet.tsx
+++ src/lib/ui/ConnectWallet.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useCallback, useEffect, useState } from 'react'
 import WalletSDK from '../wallets'
 import { Modal } from '../components/Modal'
 import clsx from 'clsx'
@@ -97,35 +97,51 @@ export const ConnectWallet = ({ visible, walletSDK, initComponent = 'Connect' }:
     setIsOpen(false)
   }
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768) // 假设768px以下为移动设备
+  const handleResize = useCallback(() => {
+    setIsMobile(window.innerWidth <= 768)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    // 清理函数
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [handleResize])
+
   const el = <SimpleRouter routes={routes} initialRouteName={initComponent} onClose={onClose} />
 
   return (
     <WalletSDKContext.Provider value={walletSDK}>
       <QueryClientProvider client={queryClient}>
-        <Sheet isOpen={isOpen} customRootId="ConnectWalletSheet" className="md:hidden">
-          <div
-            className={clsx(
-              'relative box-border w-full overflow-hidden rounded-t-[32px] bg-[unset]',
-              isOpen ? 'animation-fade-in-up' : 'animation-fade-out-down',
-            )}
-          >
-            <div className="box-border w-full overflow-hidden rounded-t-[32px] border-2 border-b-0 border-solid border-[#5262791A] bg-white">
-              {el}
+        {isMobile ? (
+          <Sheet isOpen={isOpen} customRootId="ConnectWalletSheet" className="md:hidden">
+            <div
+              className={clsx(
+                'relative box-border w-full overflow-hidden rounded-t-[32px] bg-[unset]',
+                isOpen ? 'animation-fade-in-up' : 'animation-fade-out-down',
+              )}
+            >
+              <div className="box-border w-full overflow-hidden rounded-t-[32px] border-2 border-b-0 border-solid border-[#5262791A] bg-white">
+                {el}
+              </div>
             </div>
-          </div>
-        </Sheet>
-        <Modal isOpen={isOpen} customRootId="ConnectWalletModal" className="max-md:hidden">
-          <div
-            className={clsx(
-              'relative box-border w-[92%] max-w-[400px] overflow-hidden rounded-[32px] bg-[unset]',
-              isOpen ? 'animation-fade-in-up' : 'animation-fade-out-down',
-            )}
-          >
-            <div className="box-border w-full overflow-hidden rounded-[32px] border-2 border-solid border-[#5262791A] bg-white">
-              {el}
+          </Sheet>
+        ) : (
+          <Modal isOpen={isOpen} customRootId="ConnectWalletModal" className="max-md:hidden">
+            <div
+              className={clsx(
+                'relative box-border w-[92%] max-w-[400px] overflow-hidden rounded-[32px] bg-[unset]',
+                isOpen ? 'animation-fade-in-up' : 'animation-fade-out-down',
+              )}
+            >
+              <div className="box-border w-full overflow-hidden rounded-[32px] border-2 border-solid border-[#5262791A] bg-white">
+                {el}
+              </div>
             </div>
-          </div>
-        </Modal>
+          </Modal>
+        )}
       </QueryClientProvider>
     </WalletSDKContext.Provider>
   )
